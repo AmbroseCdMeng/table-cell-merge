943481eb3e0c547f41fd931a035d1c6dd127eece
AmbroseCdMeng <32756131+AmbroseCdMeng@users.noreply.github.com>
2019-08-24 15:13:49 +0800

    Update merge.js

--- merge.js
+++ merge.js
@@ -34,7 +34,7 @@ function mergeSingleCol(tableid, colIdx) {
     });
 }
 
-/* ***** 合并某 n 列内容相同的单元格（单元格内容有关联） *****
+/* ***** 合并某 n 列(指定范围)内容相同的单元格（单元格内容有关联） *****
  * 表格合并列单元格，colIdx：要合并的前 n 列
  * @param tableid 表ID，例如#Table_Center
  * @param colIdx  结束列位置 从 0 开始 含
@@ -92,6 +92,60 @@ function mergeMultiCol(tableid, colIdx, colIdxstart) {
 }
 
 
+/* ***** 合并某 n 列(指定列索引)内容相同的单元格（单元格内容有关联） *****
+ * 表格合并列单元格，colIdx：要合并的列（数组）
+ * @param tableid 表ID，例如#Table_Center
+ * @param colArr 列数组 如 [1,2,3]
+ */
+function mergeMultiCol_2(tableid, colArr) {
+    var that;
+    if (!(colArr instanceof Array))
+        return;
+    var _text = function (cols) {
+        return $.map(cols, function (col) {
+            return $(col).text();
+        }).join("**");
+    };
+    var condition_str = $.map(colArr, (i, item) => {
+        //return "td eq(" + i + ")";
+        return `td:eq(${i})`;
+    }).join(",");
+
+    /* 遍历表格下所有行 */
+    $(tableid + " tr").each(function (row) {
+        /* 获取指定前n列 并篩選"不可見"元素（hidden、disabled 等）*/
+        var _colspan = $(this)
+            .find(condition_str)
+            .filter(":visible");
+        /* 如果存在可见列 */
+        if (_colspan.length > 0) {
+            /* 如果存在上一行内容 并且 当前行的前 colIdx 列的内容 与上一行的前 colIdx 列的内容 完全相同 */
+            if (that != null && _text(_colspan) == _text(that)) {
+                /* 遍历前当前行的 前 colIdx 列 */
+                that.each(function (col) {
+                    /* 获取当前遍历到的列的 rowSpan 属性 */
+                    rowspan = $(this).attr("rowSpan");
+                    /* 如果未定义 rowSpan 属性 */
+                    if (rowspan == undefined) {
+                        /* 对当前单元格添加 rowSpan 属性，默认值为 1 */
+                        $(this).attr("rowSpan", 1);
+                        rowspan = $(this).attr("rowSpan");
+                    }
+                    /* 如果当前单元格已存在 rowSpan 属性 ，则对 rowspan 的值加上之前合并过的 rowspan 的值（默认 为 1）*/
+                    rowspan = Number(rowspan) + Number($(_colspan).attr("rowSpan") || 1);
+                    $(this).attr("rowSpan", rowspan);
+                });
+                /* 遍历之前判断的内容相同的列，依次隐藏 */
+                $(_colspan).each(function (col) {
+                    $(this).hide();
+                });
+            } else {
+                /* 如果不存在上一行内容（第一行）*/
+                that = _colspan;
+            }
+        }
+    });
+}
 
 /* ***** 跨列合并某单元格 *****
  * 跨列合并(如果结束行传0代表合并所有行)
